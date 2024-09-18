9a95d73aeab374ee9e4deac1f8a72474441f5657
Raphael Habichler <raphael.habichler@bmd.at>
2024-09-18 09:34:53 +0200

    added script for latex creation

--- delploy.bash
+++ delploy.bash
@@ -3,4 +3,9 @@
 if [ ! -e ~/.tmux/plugins/tpm ];then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
-ln -s .tmux.conf ~/.tmux.conf
+if [ ! -e ~/.local/bin ];then
+    mkdir -p $HOME/.local/bin/
+fi
+ln -s $HOME/.config/.tmux.conf $HOME
+ln -s $HOME/.config/de $HOME/.local/bin/
+ln -s $HOME/.config/ntex $HOME/.local/bin/
